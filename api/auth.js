1d89a731d98df5827dfba37399cb5297c94f933a
Louis A <louis.aubert@monpetitgazon.com>
2022-11-20 22:57:55 +0100

    fix

--- api/auth.js
+++ api/auth.js
@@ -9,20 +9,28 @@ const pusher = new Pusher({
   useTLS: false
 });
 
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-
-  if(req.method === 'OPTIONS') {
-    return res.status(200).json(({
-        body: "OK"
-    }))
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
   }
+  return await fn(req, res)
+}
 
+const handler = async (req, res) => {
   const { socketId, channel } = req.body;
 
   const auth = pusher.authenticate(socketId, channel);
   res.send(auth);
 }
+
+module.exports = allowCors(handler);
